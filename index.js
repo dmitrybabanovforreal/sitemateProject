321a0d950b63f62510aa7f6d501aa74112a6b146
Dmitry Babanov <85852989+dmitrybabanovforreal@users.noreply.github.com>
2025-06-16 10:44:22 +0700

    extend time conversion function

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
 // expecting time to be a string in the format like '8:15' or '12:30'
+const numberWords = {
+  1: "one",
+  2: "two",
+  3: "three",
+  4: "four",
+  5: "five",
+  6: "six",
+  7: "seven",
+  8: "eight",
+  9: "nine",
+  10: "ten",
+  11: "eleven",
+  12: "twelve",
+  13: "thirteen",
+  14: "fourteen",
+  15: "fifteen",
+  16: "sixteen",
+  17: "seventeen",
+  18: "eighteen",
+  19: "nineteen",
+  20: "twenty",
+  21: "twenty one",
+  22: "twenty two",
+  23: "twenty three",
+  24: "twenty four",
+  25: "twenty five",
+  26: "twenty six",
+  27: "twenty seven",
+  28: "twenty eight",
+  29: "twenty nine",
+  30: "thirty"
+};
+
 function convertTimeToWords(time) {
   // TODO: real code goes here!
   if (time === '0:00') {
     return 'midnight';
+  } else if (time === '12:00') {
+    return 'midday';
+  } else {
+    const parts = time.split(':');
+    if (parts.length === 2) {
+      return convertHoursMinutes(parts[0], parts[1])
+    } else {
+      console.error('invalid string')
+    }
   }
+}
 
-  return 'half past eight';
+function convertHoursMinutes(hours, minutes) {
+  const minutesInt = Number(minutes);
+  if (minutesInt > 0 && minutesInt <= 30) {
+    const connector = 'past';
+    return concatenateParts(minutes, connector, hours);
+  } else if (minutesInt > 30 && minutesInt < 60) {
+    const connector = 'to';
+    return concatenateParts(String(60 - minutesInt), connector, hours);
+  } else if (minutesInt === 0) {
+    const hoursWord = numberWords[hours];
+    return hoursWord + "o'clock";
+  } else {
+    console.error()
+  }
+}
+
+function concatenateParts(minutes, connector, hours) {
+  const hoursWord = numberWords[hours];
+  const minutesWord = numberWords[minutes];
+  return minutesWord + connector + hoursWord;
 }
 
 module.exports = { convertTimeToWords };
